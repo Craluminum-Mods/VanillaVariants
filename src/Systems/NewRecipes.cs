4d7107ddbf349782c3dd616ba630942cecfd2ed0
Craluminum2413 <beware.14b@gmail.com>
2024-04-16 20:15:16 +0300

    separate classes

---
+++ src/Systems/NewRecipes.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Vintagestory.API.Common;
+using Vintagestory.ServerMods;
+
+namespace VanillaVariants;
+
+public class NewRecipes : ModSystem
+{
+    internal static long elapsedMilliseconds = 0;
+    public static int count = 0;
+
+    public static List<GridRecipe> newRecipes = new();
+
+    public override bool ShouldLoad(EnumAppSide forSide) => forSide.IsServer();
+    public override double ExecuteOrder() => 1.01;
+
+    public override void AssetsLoaded(ICoreAPI api)
+    {
+        GridRecipeLoader gridRecipeLoader = api.ModLoader.GetModSystem<GridRecipeLoader>();
+
+        foreach (GridRecipe recipe in newRecipes)
+        {
+            gridRecipeLoader.LoadRecipe(new AssetLocation("skippatch"), recipe);
+        }
+    }
+
+    public override void AssetsFinalize(ICoreAPI api)
+    {
+        api.Logger.Notification($"[{Mod.Info.Name}] RecipePatch Loader: Completed in: {elapsedMilliseconds} ms, {count} patches total");
+    }
+}
--- src/Systems/Recipes.cs
+++ src/Systems/Recipes.cs
@@ -3,35 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Vintagestory.API.Common;
 using Vintagestory.API.Util;
-using Vintagestory.ServerMods;
 
 namespace VanillaVariants;
-
-public class NewRecipes : ModSystem
-{
-    internal static long elapsedMilliseconds = 0;
-    public static int count = 0;
-
-    public static List<GridRecipe> newRecipes = new();
-
-    public override bool ShouldLoad(EnumAppSide forSide) => forSide.IsServer();
-    public override double ExecuteOrder() => 1.01;
-
-    public override void AssetsLoaded(ICoreAPI api)
-    {
-        GridRecipeLoader gridRecipeLoader = api.ModLoader.GetModSystem<GridRecipeLoader>();
-
-        foreach (GridRecipe recipe in newRecipes)
-        {
-            gridRecipeLoader.LoadRecipe(new AssetLocation("skippatch"), recipe);
-        }
-    }
-
-    public override void AssetsFinalize(ICoreAPI api)
-    {
-        api.Logger.Notification($"[{Mod.Info.Name}] RecipePatch Loader: Completed in: {elapsedMilliseconds} ms, {count} patches total");
-    }
-}
 
 public class Recipes : ModSystem
 {
