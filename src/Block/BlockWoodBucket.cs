ffc44ef0b63628b0a1646c57732ac1f2fbc955f3
Craluminum2413 <beware.14b@gmail.com>
2024-04-18 12:48:50 +0300

    declare in namespace

--- src/Block/BlockWoodBucket.cs
+++ src/Block/BlockWoodBucket.cs
@@ -1,5 +1,7 @@
 using Vintagestory.GameContent;
 
+namespace VanillaVariants;
+
 public class BlockWoodBucket : BlockBucket
 {
     protected override string meshRefsCacheKey => "bucketMeshRefs" + this.Code;
