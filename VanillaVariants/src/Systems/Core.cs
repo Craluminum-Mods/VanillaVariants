c39cb5d5628e45496772f06e697b27620c4ff0f8
Dana <69315569+Craluminum2413@users.noreply.github.com>
2025-12-24 23:54:40 +0200

    use attribute system for sign post

--- VanillaVariants/src/Systems/Core.cs
+++ VanillaVariants/src/Systems/Core.cs
@@ -81,6 +81,7 @@ public class Core : ModSystem
         AddBehaviorWithPropertiesIfTrue(Config.Ladder && obj.Code.PathStartsWith("ladder-wood"), obj, behaviorProps["ladder"]);
         AddBehaviorWithPropertiesIfTrue(Config.Shelf && obj is BlockShelf && obj.Code.PathStartsWith("shelf-normal"), obj, behaviorProps["shelf"]);
         AddBehaviorWithPropertiesIfTrue(Config.Sign && obj is BlockSign && obj.Code.PathStartsWith("sign"), obj, behaviorProps["sign"]);
+        AddBehaviorWithPropertiesIfTrue(Config.Sign && obj is BlockSignPost && obj.Code.PathStartsWith("signpost"), obj, behaviorProps["signpost"]);
     }
 
     private void AddBehaviorWithPropertiesIfTrue(bool condition, CollectibleObject obj, JsonObject props)
@@ -141,6 +142,7 @@ public class Core : ModSystem
         behaviorProps["ladder"] = JsonObject.FromJson(api.Assets.TryGet(AssetLocation.Create("vanvar:config/forcedpatches/ladder-properties.json")).ToText());
         behaviorProps["shelf"] = JsonObject.FromJson(api.Assets.TryGet(AssetLocation.Create("vanvar:config/forcedpatches/shelf-properties.json")).ToText());
         behaviorProps["sign"] = JsonObject.FromJson(api.Assets.TryGet(AssetLocation.Create("vanvar:config/forcedpatches/sign-properties.json")).ToText());
+        behaviorProps["signpost"] = JsonObject.FromJson(api.Assets.TryGet(AssetLocation.Create("vanvar:config/forcedpatches/signpost-properties.json")).ToText());
     }
 
     public override void Dispose()
---
+++ VanillaVariants/src/Systems/HarmonyPatches/AttributeRenderingPatches/SignPostPatches.cs
@@ -0,0 +1,81 @@
+﻿using HarmonyLib;
+using System;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+using Vintagestory.GameContent;
+
+namespace VanillaVariants;
+
+[HarmonyPatch]
+public static partial class AttributeRenderingPatches
+{
+    [HarmonyPatchCategory("Client")]
+    [HarmonyPatch(typeof(BlockEntitySignPost), nameof(BlockEntitySignPost.OnTesselation))]
+    public static class SignPost_OnTesselation_Patch
+    {
+        public static bool Prefix(BlockEntitySignPost __instance, ref bool __result, ITerrainMeshPool mesher, ITesselatorAPI tessThreadTesselator, string[] ___textByCardinalDirection, MeshData ___signMesh)
+        {
+            if (Core.Config?.Signpost == false)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (___textByCardinalDirection[i].Length != 0)
+                {
+                    Cardinal obj = Cardinal.ALL[i];
+                    float rotY = 0f;
+                    switch (obj.Index)
+                    {
+                        case 0:
+                            rotY = 180f;
+                            break;
+                        case 1:
+                            rotY = 135f;
+                            break;
+                        case 2:
+                            rotY = 90f;
+                            break;
+                        case 3:
+                            rotY = 45f;
+                            break;
+                        case 5:
+                            rotY = 315f;
+                            break;
+                        case 6:
+                            rotY = 270f;
+                            break;
+                        case 7:
+                            rotY = 225f;
+                            break;
+                    }
+                    mesher.AddMeshData(___signMesh.Clone().Rotate(new Vec3f(0.5f, 0.5f, 0.5f), 0f, rotY * ((float)Math.PI / 180f), 0f));
+                }
+            }
+
+            for (int i = 0; i < __instance.Behaviors.Count; i++)
+            {
+                __instance.Behaviors[i].OnTesselation(mesher, tessThreadTesselator);
+            }
+
+            __result = true;
+            return false;
+        }
+    }
+
+    [HarmonyPatchCategory("Universal")]
+    [HarmonyPatch(typeof(BlockSignPost), nameof(BlockSignPost.TryPlaceBlock))]
+    public static class SignPost_TryPlaceBlock_Patch
+    {
+        public static void Postfix(BlockSignPost __instance, ref bool __result, ItemStack itemstack, BlockSelection bs)
+        {
+            if (Core.Config?.Signpost == false || !__result)
+            {
+                return;
+            }
+            (__instance?.GetBEBehavior<AttributeRenderingLibrary.BlockEntityBehaviorShapeTexturesFromAttributes>(bs.Position))?.OnBlockPlaced(itemstack);
+        }
+    }
+}
